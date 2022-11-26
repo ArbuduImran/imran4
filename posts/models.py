443a185fc70447948372b534adc1ed81c3b40afd
ASANOVV <mirbek020295@gmail.com>
2022-11-26 16:25:49 +0300

    homework 5

--- posts/models.py
+++ posts/models.py
@@ -15,8 +15,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField()
-    likes = models.IntegerField()
+    image = models.ImageField(blank=True, null=True)
+    likes = models.IntegerField(default=0)
     hashtags = models.ManyToManyField(Hashtag, null=True, blank=True)
 
     def __str__(self):
